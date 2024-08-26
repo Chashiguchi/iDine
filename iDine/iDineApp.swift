2ec977d396810a3c3aec4ccbb7c6533762495988
Chase <Chashiguchi@gmail.com>
2024-08-26 10:05:16 -0500

    Initial Commit

---
+++ iDine/iDineApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDineApp.swift
+//  iDine
+//
+//  Created by Chase Hashiguchi on 8/26/24.
+//
+
+import SwiftUI
+
+@main
+struct iDineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
