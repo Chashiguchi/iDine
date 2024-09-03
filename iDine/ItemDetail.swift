a64a1b016a2669d52188c869eda2b85a978e7009
Chase <Chashiguchi@gmail.com>
2024-09-03 09:52:40 -0500

    finished adding items to an order with @EnvironmentObject

--- iDine/ItemDetail.swift
+++ iDine/ItemDetail.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct ItemDetail: View {
     let item: MenuItem
+    @EnvironmentObject var order: Order
     var body: some View {
         VStack {
             ZStack(alignment: .bottomTrailing) {
@@ -24,6 +25,10 @@ struct ItemDetail: View {
             }
             Text(item.description)
                 .padding()
+            Button("Order This") {
+                order.add(item: item)
+            }
+            .buttonStyle(.borderedProminent)
             Spacer()
         }
         .navigationTitle(item.name)
@@ -35,5 +40,6 @@ struct ItemDetail: View {
 #Preview {
     NavigationStack {
         ItemDetail(item: MenuItem.example)
+            .environmentObject(Order())
     }
 }
